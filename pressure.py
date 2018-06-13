b05b0d9fd7b9689f611ba2950c5bb35e6e7948b2
MVerghese <mtverghe@ucsd.edu>
2018-06-13 16:42:45 -0700

    Fixed pressure calculation

--- pressure.py
+++ pressure.py
@@ -4,24 +4,32 @@ def getCollisionPressure(boundingBoxList):
 		out.append(box[4])
 	return(out)
 
-def getParticlePressure(boundingBoxList, particleStates):
+def getParticlePressure(boundingBoxList, particleStates, particleMomentums):
 	particleTally = []
+	particleMomentumTotal = []
 	for box in boundingBoxList:
 		particleTally.append(0)
+		particleMomentumTotal.append(0)
 
-	for particle in particleStates:
+	for p in range(0,len(particleStates)):
 		for i in range(0,len(boundingBoxList)):
-			if(particle[0] >= boundingBoxList[i][0] and particle[1] >= boundingBoxList[i][1] and particle[0] <= boundingBoxList[i][2] and particle[1] <= boundingBoxList[i][3]):
+			if(particleStates[p][0] >= boundingBoxList[i][0] and particleStates[p][1] >= boundingBoxList[i][1] and particleStates[p][0] <= boundingBoxList[i][2] and particleStates[p][1] <= boundingBoxList[i][3]):
 				particleTally[i] += 1
+				particleMomentumTotal[i] += (particleMomentums[p][0] ** 2 + particleMomentums[p][1] ** 2)
 				break
 
-	return(particleTally)
+	out = []
+	for i in range(0,len(particleMomentumTotal)):
+		out.append(particleMomentumTotal[i]/particleTally[i])
+
+	return(out)
 
 def main():
 	boundingBoxList = [[0,0,1,1,5],[1,0,2,1,7]]
 	particleStates = [[0,0],[.5,.5],[.7,.7],[1,.5],[1.3,.3],[1.6,.6]]
+	particleMomentums = [[0,0],[.5,.5],[.7,.7],[1,.5],[1.3,.3],[1.6,.6]]
 	print(getCollisionPressure(boundingBoxList))
-	print(getParticlePressure(boundingBoxList,particleStates))
+	print(getParticlePressure(boundingBoxList,particleStates,particleMomentums))
 
 main()
 
